7c1352d07d239af14e1adfc80a1dde9e192f16c0
lordxes <vvvvipdarlin@gmail.com>
2024-03-17 00:57:22 +0800

    实现文件IO的滑动时间窗格限流

--- src/TimeWindowLimitter.php
+++ src/TimeWindowLimitter.php
@@ -11,12 +11,15 @@ class TimeWindowLimitter implements LimitterInterface
 {
 
     /**
+     * 滑动时间窗格限流器
      * @param int $limit 限制频次
      * @param int $second 限制时间
      * @param string $key 限制颗粒度
      * @param string $driver
      * @param object $handler 缓存处理器
+     * @return bool
      * @throws RedisException
+     * @throws \Exception
      */
     public function setLimit(int $limit, int $second, string $key, string $driver, $handler)
     {
@@ -25,25 +28,28 @@ class TimeWindowLimitter implements LimitterInterface
             return throw new \InvalidArgumentException("目前仅支持Redis和File作为限流驱动");
         }
 
+        $ret = false;
+
         switch ($driver) {
             case "redis":
                 $ret = RedisStorage::setTimeWindowLimit($limit, $second, $key, $handler);
-
-                // Lua脚本异常
-                if ($ret === false) {
-                    throw new RedisException("error: Lua execute fail, Internal Error Occur!");
-                }
-
-                // 未超出流量允许放行
-                if ($ret === 1) {
-                    return true;
-                }
-
-                // 超出流量
-                return false;
+                break;
             case "file":
                 $ret = FileStorage::setTimeWindowLimit($limit, $second, $key, $handler);
-                return false;
+                break;
         }
+
+        // Lua脚本异常
+        if ($ret === false) {
+            throw new RedisException("error: Lua execute fail, Internal Error Occur!");
+        }
+
+        // 未超出流量允许放行
+        if ($ret === 1) {
+            return true;
+        }
+
+        // 超出流量
+        return false;
     }
 }
